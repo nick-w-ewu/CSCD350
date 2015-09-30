06ec00a8f4a17f7e5d41b57dab55a19f14ed3418
nick-w-ewu <nicholas.witmer@eagles.ewu.edu>
2015-09-29 21:42:21 -0700

    Standards
    Conforming to coding standards and added documentation

--- Assignment 2/src/Field.java
+++ Assignment 2/src/Field.java
@@ -1,9 +1,26 @@
+/**
+ * Field.java
+ * Author: Nicholas Witmer
+ *Revision: 1, Nicholas Witmer
+ *Date 09/27/2015
+ *Used to store an array of integers representing a minefield and also offers functionality
+ *to solve the minefield and return a string representation of the minefield
+ *
+ */
 
 public class Field
 {
 	private int rows;
 	private int columns;
 	private int[][] field;
+	
+	/*
+	 * Initializes a new Field with the parameters passed in
+	 * Parameters:
+	 * Int r - number of rows
+	 * Int c - number of columns
+	 * Int[][] field - the minefield that this Field object is representing
+	 */
 	
 	public Field(int r, int c, int[][] field)
 	{
@@ -12,83 +29,110 @@ public class Field
 		this.field = field;
 	}
 	
+	/*
+	 * This method is called by with a Field object, it traverses the field visiting each element
+	 * if the element is not a mine it searches its 8 neighbors increasing the count each time a 
+	 * mine is found in one of its neighbors
+	 * 
+	 */
+	
 	public void solveFeild()
 	{
-		for(int r = 0; r < this.rows; r++)
+		for(int row = 0; row < this.rows; row++)
 		{
-			for(int c = 0; c < this.columns; c++)
+			for(int col = 0; col < this.columns; col++)
 			{
-				if(this.field[r][c] != -1)
+				if(this.field[row][col] != -1)
 				{
-					if(isMine(r, c + 1))
+					if(isMine(row, col + 1))
 					{
-						this.field[r][c] += 1;
-					}
-					if(isMine(r, c - 1))
+						this.field[row][col] += 1;
+					}//end if
+					if(isMine(row, col - 1))
 					{
-						this.field[r][c] += 1;
-					}
-					if(isMine(r + 1, c))
+						this.field[row][col] += 1;
+					}//end if
+					if(isMine(row + 1, col))
 					{
-						this.field[r][c] += 1;
-					}
-					if(isMine(r - 1, c))
+						this.field[row][col] += 1;
+					}//end if
+					if(isMine(row - 1, col))
 					{
-						this.field[r][c] += 1;
-					}
-					if(isMine(r + 1, c + 1))
+						this.field[row][col] += 1;
+					}//end if
+					if(isMine(row + 1, col + 1))
 					{
-						this.field[r][c] += 1;
-					}
-					if(isMine(r - 1, c + 1))
+						this.field[row][col] += 1;
+					}//end if
+					if(isMine(row - 1, col + 1))
 					{
-						this.field[r][c] += 1;
-					}
-					if(isMine(r - 1, c - 1))
+						this.field[row][col] += 1;
+					}//end if
+					if(isMine(row - 1, col - 1))
 					{
-						this.field[r][c] += 1;
-					}
-					if(isMine(r + 1, c - 1))
+						this.field[row][col] += 1;
+					}//end if
+					if(isMine(row + 1, col - 1))
 					{
-						this.field[r][c] += 1;
-					}
-				}
-			}
-		}
+						this.field[row][col] += 1;
+					}//end if
+				}//end if
+			}//end for
+		}//end for
 	}
 	
-	private boolean isMine(int i, int j)
+	/*
+	 * Checks to see if the passed in Field element is actually in the field dimensions
+	 * and if it is returns whether that element contains a mine, denoted by a -1
+	 * 
+	 * Parameters:
+	 * Int row - row in the field
+	 * Int col - column in the field
+	 * 
+	 * Returns:
+	 * Boolean - true if the field element exists and is a mine, false otherwise
+	 */
+	
+	private boolean isMine(int row, int col)
 	{
-		if(i >= this.rows || j >= this.columns || i < 0 || j <0)
+		if(row >= this.rows || col >= this.columns || row < 0 || col <0)
 		{
 			return false;
-		}
-		if(this.field[i][j] == -1)
+		}//end if
+		if(this.field[row][col] == -1)
 		{
 			return true;
-		}
+		}//end if
 		return false;
 	}
+	
+	/*
+	 * Creates a string representation of a minefield, with a mine represented by an *
+	 * and all other positions with the number contained in the field element
+	 * 
+	 * Returns:
+	 * String - String representation of the minefield
+	 */
 	
 	public String toString()
 	{
 		String toReturn = "";
 		
-		for(int r = 0; r < this.rows; r++)
+		for(int row = 0; row < this.rows; row++)
 		{
-			for(int c = 0; c < this.columns; c++)
+			for(int col = 0; col < this.columns; col++)
 			{
-				if(this.field[r][c] == -1)
+				if(this.field[row][col] == -1)
 				{
 					toReturn += "*";
-				}
+				}//end if
 				else
 				{
-					toReturn += this.field[r][c];
-				}
-			}
+					toReturn += this.field[row][col];
+				}//end else
+			}//end for
 			toReturn += "\n";
-		}
+		}//end for
 		return toReturn;
 	}
 }
--- Assignment 2/src/Minesweeper.java
+++ Assignment 2/src/Minesweeper.java
@@ -1,17 +1,35 @@
+/**
+ * Field.java
+ * Author: Nicholas Witmer
+ *Revision: 1, Nicholas Witmer
+ *Date 09/27/2015
+ *Accepts minefields input by the user from the console with the number of rows and number
+ *of columns specified first followed by the minefield itself. Input fields are accepted parsed
+ *into 2d arrays with a -1 specifying the location of a mine and all other field positions
+ *starting out a a 0. These arrays are stored in Field objects and then stored in an 
+ *ArrayList. When the user enters 0 0 for the field dimensions all the fields in the
+ *ArrayList are solved and then printed to the console. 
+ *
+ */
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
 public class Minesweeper
 {
-
+	/*
+	 * Main method accepts minefields input by the user in the console, parses them into
+	 * Field objects, stores them, and when the user is done the minefields are passed
+	 * on to the printFields method to be solved and printed
+	 */
+	
 	public static void main(String[] args)
 	{
-		int rows, cols, fieldNum = 1;
+		int rows, cols;
 		String line = "";
 		int[][] inputFeild;
 		Field tempFeild;
-		ArrayList<Field> minefeilds = new ArrayList<Field>();
+		ArrayList<Field> minefields = new ArrayList<Field>();
 		Scanner input = new Scanner(System.in);
 		
 		rows = input.nextInt();
@@ -30,26 +48,41 @@ public class Minesweeper
 					if(line.charAt(c) == '*')
 					{
 						inputFeild[r][c] = -1;
-					}
+					}//end if
 					else
 					{
 						inputFeild[r][c] = 0;
-					}
-				}
-			}
+					}//end else
+				}//end for
+			}//end for
 			tempFeild = new Field(rows, cols, inputFeild);
-			minefeilds.add(tempFeild);
+			minefields.add(tempFeild);
 			rows = input.nextInt();
 			cols = input.nextInt();
-		}
-		for(Field i : minefeilds)
+		}//end while
+		printFields(minefields);
+	}
+	
+	/*
+	 * Loops through all the minefields in the passed in ArrayList, solves them, and
+	 * then prints them to the console along with their field number
+	 * 
+	 * Parameters:
+	 * ArraList<Field> minefields - all the minefields entered by the user stored as 
+	 * field objects
+	 */
+	
+	public static void printFields(ArrayList<Field> minefields)
+	{
+		int fieldNum = 1;
+		for(Field i : minefields)
 		{
 			System.out.println("Field #" + fieldNum);
 			i.solveFeild();
 			System.out.println(i);
 			fieldNum++;
 			System.out.println();
-		}
+		}//end for
 	}
 
 }
